45877dc02e40c805f6030a692743563b68b89021
Manu Chandel <manuchandel@users.noreply.github.com>
2015-08-11 11:25:47 +0530

    Create SIMPLIFYPATH.cpp

---
+++ stacks-And-Queues/SIMPLIFYPATH.cpp
@@ -0,0 +1,57 @@
+string Solution::simplifyPath(string A) {
+    stack<string> mystack;
+    int n=A.length();
+    string temp="";
+    bool path=0;
+    int i,j;
+    for(i=0;i<n;i++){
+        if(A[i]=='/'){
+            path=1;
+            if(temp.length()>2)
+                path=0;
+            for(j=0;j<temp.length() && path;j++){
+                if(temp[j]!='.')
+                    path=0;
+            }
+            if(!path)
+                mystack.push(temp);
+            else{
+                if(temp.length()==2){
+                    if(!mystack.empty())
+                        mystack.pop();
+                }
+            }
+            temp="";
+        }
+        else temp+=A[i];
+    }
+    path=1;
+    if(temp.length()>2)
+        path=0;
+    for(j=0;j<temp.length() && path;j++){
+        if(temp[j]!='.')
+            path=0;
+    }
+    if(!path)
+        mystack.push(temp);
+    else{
+        if(temp.length()==2){
+            if(!mystack.empty())
+                mystack.pop();
+        }
+    }
+    stack<string> ans;
+    while(!mystack.empty()){
+        ans.push(mystack.top());
+        mystack.pop();
+    }
+    string s="";
+    while(!ans.empty()){
+        s+="/";
+        s+=ans.top();
+        ans.pop();
+    }
+    if(s.length()==0)
+        s="/";
+    return s;
+}
