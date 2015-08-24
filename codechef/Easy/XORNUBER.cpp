140f6d32784a829ee049273e1cc7508b81094370
Manu Chandel <manuchandel@users.noreply.github.com>
2015-08-25 00:06:51 +0530

    Create XORNUBER.cpp

---
+++ codechef/Easy/XORNUBER.cpp
@@ -0,0 +1,24 @@
+#include <cstdio>
+#define FOR(a,b) for(int a=0;a<b;a++)
+using namespace std;
+int xorVal(int A){
+  if(A%2==0)
+    return -1;
+  if(A==1)
+    return 2;
+  A=A+1;
+  if(!(A&(A-1))){
+    A=A-1;
+    A=A>>1;
+    return A;
+  }else return -1;
+} 
+int main()
+{
+  int T,A;
+  scanf("%d",&T);
+  while(T--){
+    scanf("%d",&A);
+    printf("%d\n",xorVal(A));
+  }
+}
